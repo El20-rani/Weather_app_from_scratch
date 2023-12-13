33bc6e40f036f4037ddd86d31f6f8134ecbf84d0
Eleni Zewdu <81524686+El20-rani@users.noreply.github.com>
2023-12-13 19:28:20 +0530

    updated the search engine with api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
+function updateWeather(response) {
+  let temperature = document.querySelector("#temp-value");
+  let city_elt = document.querySelector("#city");
+  city_elt.innerHTML = response.data.city;
+  temperature.innerHTML = Math.round(response.data.temperature.current);
+}
+
+function searchCity(city) {
+  let apiKey = `893fc75a240abbc6oc34eddd57f08bta`;
+  let url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(url).then(updateWeather);
+}
+
 function search(event) {
   event.preventDefault();
   let search_input = document.querySelector("#search-input");
-  let city_elt = document.querySelector("#city");
-  city_elt.innerHTML = search_input.value;
+  searchCity(search_input.value);
 }
 let form_elt = document.querySelector("#search-form");
 form_elt.addEventListener("submit", search);
+
+searchCity("Addis Ababa");
